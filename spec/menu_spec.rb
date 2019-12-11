8a7295afba73d56aaab0884940dde834ddd708da
Daniel Fumero Cruz <alu0100996081@ull.edu.es>
2019-12-11 17:07:35 +0000

    Prueba huella max

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,17 +5,17 @@ require 'gema/plato_hijo.rb'
 
 RSpec.describe Plato do
 	before :each do
-		@a1 = Alimentos.new("Potaje",43,21,54,3,4)
-		@a2 = Alimentos.new("Carne",23,23,54,6,3)
-		@a3 = Alimentos.new("Helado",434,54,3,5,4)
+		@a1 = Alimentos.new("Potaje",20,40,40,24,20)
+		@a2 = Alimentos.new("Carne",15,35,50,31,37)
+		@a3 = Alimentos.new("Helado",25,45,30,56,12)
 	
-                @a4 = Alimentos.new("Potaje",43,21,54,3,4)
-		@a5 = Alimentos.new("Carne",23,23,54,6,3)
-		@a6 = Alimentos.new("Helado",434,54,3,5,4) 
+                @a4 = Alimentos.new("Sopa",20,65,15,65,42)
+		@a5 = Alimentos.new("huevos",15,66,25,34,81)
+		@a6 = Alimentos.new("yogurt",10,55,35,13,63) 
 
-		@a7 = Alimentos.new("Potaje",43,21,54,3,4)
-		@a8 = Alimentos.new("Carne",23,23,54,6,3)
-		@a9 = Alimentos.new("Helado",434,54,3,5,4) 
+		@a7 = Alimentos.new("cocido",20,40,40,32,10)
+		@a8 = Alimentos.new("gazpacho",15,35,50,12,31)
+		@a9 = Alimentos.new("natilla",30,60,20,21,61) 
 
 		@dieta = List.new(nil,nil)
 		@dieta.insert_tail(@a1)
@@ -52,25 +52,28 @@ RSpec.describe Plato do
 		  @cantidad3.insert_tail(26)
 		  @cantidad3.insert_tail(27)
 		  
-		  @plato3 = Plato.new("plato3",@dieta3,@cantidad3)
-
+		 @plato3 = Plato.new("plato3",@dieta3,@cantidad3)
+		 @plato_hijo3 = Plato_hijo.new("phijo3",@dieta3,@cantidad3)
 	end
 
 	describe "Prueba Huella Nutricional" do
 		it "funcion huella nutricional" do 
-			expect(@plato.huella_nutricional).to eq(2.0)
+			expect(@plato_hijo.huella_nutricional).to eq(2.0)
 		end
+
 
 		it "Plato maxima huella" do
-			expect(@plato_hijo>@plato_hijo2).to eq(false)
+			array_menu = []
+			array_menu << @plato_hijo
+			array_menu << @plato_hijo2
+			array_menu << @plato_hijo3
+			expect(array_menu.max).to eq(@plato_hijo2)
+		end
+		
+		it "Incrementa Precio" do
+
+
 		end
 
-		it ""	do
-			array_menu = []
-			array_menu << @plato
-			array_menu << @plato2
-			array_menu << @plato3
-			
-		end
 	end
 end
