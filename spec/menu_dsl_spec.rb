d9dcdb28b8973b93fe06d289167dbb867021638a
Daniel Fumero Cruz <alu0100996081@ull.edu.es>
2019-12-23 12:10:54 +0000

    Prueba menu dsl

---
+++ spec/menu_dsl_spec.rb
@@ -0,0 +1,23 @@
+
+
+
+Rspec.describe MenuDsl do
+
+	describe "carga Correctamente" do
+		it "ejemplo de carga" do
+			menu = MenuDsl.new("Lunes") do
+				name = "test"
+				desayuno :descripcion => "tarta",
+					:porcion => "1 porcion",
+					:gramos => 100.0,
+					:grasas => 32.2,
+					:carbohidratos => 54.2,
+					:proteinas => 11.0,
+					:fibra => 2.3,
+					:sal =>  0.06
+			end
+
+			puts menu.to_s
+		end
+	end
+end
