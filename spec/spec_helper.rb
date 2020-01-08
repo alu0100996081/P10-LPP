929326e59cdcb08d2948580b2ad33a679c0792d8
alu0100996081 <45066806+alu0100996081@users.noreply.github.com>
2020-01-08 09:07:16 +0000

    prueba 2 spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require "gema"
 require 'coveralls'
 
+Coveralls.wear! 
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
